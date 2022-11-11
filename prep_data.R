45dab5e64e9fd9f6585be7f400c64780d2ae3819
Karl Broman <kbroman@gmail.com>
2022-11-11 09:24:53 -0600

    prep_data.R: reduce salaries.json size by pulling out titles and divisions
    - titles and divisions have a lot of duplicated text; putting those in
      separate files and just using integers or job codes

--- prep_data.R
+++ prep_data.R
@@ -18,10 +18,34 @@ z <- apply(x, 1, paste, collapse="|")
 m <- match(z, z)
 x <- x[unique(m),]
 
-# convert to JSON
-y <- jsonlite::toJSON(x)
-cat(y, file="salaries.json")
-
 # save unique Division
 div <- sort(unique(x$Division))
 cat(div, file="divisions.json")
+
+# replace division with division number (1 - no. divisions)
+x$Division <- match(x$Division, div)
+
+# force job codes to have unique titles
+tab <- table(x$JobCode, x$Title)
+dups <- which(rowSums(tab>0)>1)
+if(length(dups) > 0) {
+    for(dup in dups) {
+        titles <- colnames(tab)[tab[dup,]>0]
+        for(j in seq(along=titles)[-1]) {
+            x$JobCode[x$Title==titles[j]] <- paste0(x$JobCode[x$Title==titles[j]][1], letters[j])
+        }
+    }
+}
+
+# save jobcode -> title relationship
+ujobcode <- unique(x$JobCode)
+utitles <- x$Title[match(ujobcode, x$JobCode)]
+titles <- setNames(as.list(utitles), ujobcode)
+titles <- jsonlite::toJSON(titles, auto_unbox=TRUE)
+cat(titles, file="titles.json")
+
+x <- x[, colnames(x) != "Title"]
+
+# convert to JSON
+y <- jsonlite::toJSON(x)
+cat(y, file="salaries.json")
